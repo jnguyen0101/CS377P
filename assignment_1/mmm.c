5a5f098745c0a5e51cccc54be020a0ef8cbccd5b
jnguyen0101 <nguyenjenny01012006@gmail.com>
2026-01-31 18:29:36 -0600

    added output code for papi and clocktime

--- assignment_1/mmm.c
+++ assignment_1/mmm.c
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-#define __USE_POSIX199309
 #include <time.h>
 
 #define NUM_SIZES 8
 #define NUM_VARIANTS 6
+#define CACHE_FLUSH_SIZE (64 * 1024 * 1024)
 
 int sizes[NUM_SIZES] = {50, 100, 200, 400, 800, 1200, 1600, 2000};
 char *variants[NUM_VARIANTS] = {"ijk", "jik", "jki", "kji", "ikj", "kij"};
@@ -25,6 +24,7 @@ void kij(int n, complex double *A, complex double *B, complex double *C);
 void run_mmm_PAPI(mmm_fun fun, int n, char* variant);
 void populate_matrices(int n, complex double *A, complex double *B,
                        complex double *C);
+void flush_cache();
 
 
 // Matrix multiplication implementations with different loop orderings
@@ -135,26 +135,35 @@ void run_mmm_PAPI(mmm_fun fun, int n, char* variant) {
     };
 
     // Create Event Sets
-    PAPI_create_eventset(&EventSet1);
-    PAPI_add_events(EventSet1, events_1, 4);
-    PAPI_create_eventset(&EventSet2);
-    PAPI_add_events(EventSet2, events_2, 4);
-    long long counter_values_1[4];
-    long long counter_values_2[4];
+    if (PAPI_create_eventset(&EventSet1) != PAPI_OK)
+        printf("Error creating PAPI Event Set 1\n");
+    if (PAPI_add_events(EventSet1, events_1, 4) != PAPI_OK)
+        printf("Error adding events to PAPI Event Set 1\n");
+    if (PAPI_create_eventset(&EventSet2) != PAPI_OK)
+        printf("Error creating PAPI Event Set 2\n");
+    if (PAPI_add_events(EventSet2, events_2, 4) != PAPI_OK)
+        printf("Error adding events to PAPI Event Set 2\n");
+    long long counter_values_1[4] = {0};
+    long long counter_values_2[4] = {0};
 
     // Run MMM variant with PAPI measurements
     populate_matrices(n, A, B, C);
-    PAPI_start(EventSet1);
+    flush_cache();
+    if (PAPI_start(EventSet1) != PAPI_OK)
+        printf("Error starting PAPI Event Set 1\n");
     fun(n, A, B, C);
-    PAPI_stop(EventSet1, counter_values_1);
+    if (PAPI_stop(EventSet1, counter_values_1) != PAPI_OK)
+        printf("Error stopping PAPI Event Set 1\n");
 
     populate_matrices(n, A, B, C);
-    PAPI_start(EventSet2);
+    flush_cache();
+    if (PAPI_start(EventSet2) != PAPI_OK)
+        printf("Error starting PAPI Event Set 2\n");
     fun(n, A, B, C);
-    PAPI_stop(EventSet2, counter_values_2);
+    if (PAPI_stop(EventSet2, counter_values_2) != PAPI_OK)
+        printf("Error stopping PAPI Event Set 2\n");
 
     // Print results
-    printf("PAPI Results:\n");
     printf("MMM Variant: %s, Size: %d\n", variant, n);
     printf("Total Cycles: %lld\n", counter_values_1[0]);
     printf("Total Instructions: %lld\n", counter_values_1[1]);
@@ -167,10 +176,14 @@ void run_mmm_PAPI(mmm_fun fun, int n, char* variant) {
     printf("--------------------------------------------------\n");
 
     // Clean up
-    PAPI_cleanup_eventset(EventSet1);
-    PAPI_cleanup_eventset(EventSet2);
-    PAPI_destroy_eventset(&EventSet1);
-    PAPI_destroy_eventset(&EventSet2);
+    if (PAPI_cleanup_eventset(EventSet1) != PAPI_OK)
+        printf("Error cleaning up PAPI Event Set 1\n");
+    if (PAPI_cleanup_eventset(EventSet2) != PAPI_OK)
+        printf("Error cleaning up PAPI Event Set 2\n");
+    if (PAPI_destroy_eventset(&EventSet1) != PAPI_OK)
+        printf("Error destroying PAPI Event Set 1\n");
+    if (PAPI_destroy_eventset(&EventSet2) != PAPI_OK)
+        printf("Error destroying PAPI Event Set 2\n");
     free(A); free(B); free(C);
 }
 
@@ -183,25 +196,53 @@ void run_mmm_clocktime(mmm_fun fun, int n, char* variant) {
     complex double *B = (complex double*)malloc(n * n * sizeof(complex double));
     complex double *C = (complex double*)malloc(n * n * sizeof(complex double));
 
-    // Run MMM variant with clock time measurement
-    struct timespec start, end;
+    // Run MMM variant with clock cputime measurement
+    struct timespec start_c, end_c;
     populate_matrices(n, A, B, C);
-    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
+    flush_cache();
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start_c);
     fun(n, A, B, C);
-    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &end);
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &end_c);
 
     // Calculate CPU time used
-    double cpu_time_used = (end.tv_sec - start.tv_sec) +
-                           (end.tv_nsec - start.tv_nsec) / 1e9;
+    double cpu_time_used = (end_c.tv_sec - start_c.tv_sec) +
+                           (end_c.tv_nsec - start_c.tv_nsec) / 1e9;
 
     // Print results
-    printf("Clock Time Results:\n");
     printf("MMM Variant: %s, Size: %d\n", variant, n);
     printf("CPU Time Used: %f seconds\n", cpu_time_used);
     printf("--------------------------------------------------\n");
 
+    // Run MMM variant with clock realtime measurement
+    struct timespec start_r, end_r;
+    populate_matrices(n, A, B, C);
+    flush_cache();
+    clock_gettime(CLOCK_REALTIME, &start_r);
+    fun(n, A, B, C);
+    clock_gettime(CLOCK_REALTIME, &end_r);
+
+    // Calculate CPU time used
+    double real_time_used = (end_r.tv_sec - start_r.tv_sec) +
+                           (end_r.tv_nsec - start_r.tv_nsec) / 1e9;
+
+    // Print results
+    printf("MMM Variant: %s, Size: %d\n", variant, n);
+    printf("Real Time Used: %f seconds\n", real_time_used);
+    printf("--------------------------------------------------\n");
+
     // Clean up
     free(A); free(B); free(C);
+}
+
+/**
+ * Flushes the CPU cache by allocating and accessing a large buffer
+ */
+void flush_cache() {
+    volatile char *buffer = (char *)malloc(CACHE_FLUSH_SIZE);
+    for (size_t i = 0; i < CACHE_FLUSH_SIZE; i++) {
+        buffer[i] = (char)i;
+    }
+    free((void *)buffer);
 }
 
 int main(int argc, char *argv[]) {
@@ -221,6 +262,7 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(argv[1], "papi") == 0) {
         // Run PAPI measurements
+        printf("PAPI Results:\n");
         for (int i = 0; i < NUM_SIZES; i++) {
             for (int j = 0; j < NUM_VARIANTS; j++) {
                 run_mmm_PAPI(mmm_variants[j], sizes[i], variants[j]);
@@ -228,6 +270,7 @@ int main(int argc, char *argv[]) {
         }
     } else if (strcmp(argv[1], "clocktime") == 0) {
         // Run clocktime measurements
+        printf("Clock Time Results:\n");
         for (int i = 0; i < NUM_SIZES; i++) {
             for (int j = 0; j < NUM_VARIANTS; j++) {
                 run_mmm_clocktime(mmm_variants[j], sizes[i], variants[j]);
